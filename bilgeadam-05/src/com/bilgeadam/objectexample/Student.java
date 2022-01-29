84cc30341570d76dbf461478e3624a84d6cf11f2
Koray Guney <koray.guney@hotmail.com>
2022-01-29 13:05:45 +0300

    sixth week commit

---
+++ bilgeadam-05/src/com/bilgeadam/objectexample/Student.java
@@ -0,0 +1,97 @@
+package com.bilgeadam.objectexample;
+
+public class Student implements Cloneable {
+    // variables (state)
+    private String firstName;
+    private String secondName;
+    private int age;
+    private long ssid;
+    private String gender;
+
+    // constructors
+    public Student() {
+    }
+
+    public Student(String firstName, String secondName) {
+        this.firstName = firstName;
+        this.secondName = secondName;
+    }
+
+    public Student(String firstName, String secondName, int age, long ssid, String gender) {
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.age = age;
+        this.ssid = ssid;
+        this.gender = gender;
+    }
+
+    // custom methods
+
+    // getter & setter
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public long getSsid() {
+        return ssid;
+    }
+
+    public void setSsid(long ssid) {
+        this.ssid = ssid;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    // toString
+    @Override
+    public String toString() {
+        return "Student{" +
+                "firstName='" + firstName + '\'' +
+                ", secondName='" + secondName + '\'' +
+                ", age=" + age +
+                ", ssid=" + ssid +
+                ", gender='" + gender + '\'' +
+                '}';
+    }
+
+    // equals
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return ssid == student.ssid;
+    }
+
+    @Override
+    protected Student clone() throws CloneNotSupportedException {
+        return (Student) super.clone();
+    }
+}
+
---
+++ bilgeadam-05/src/com/bilgeadam/objectexample/TestStudent.java
@@ -0,0 +1,16 @@
+package com.bilgeadam.objectexample;
+
+public class TestStudent {
+    public static void main(String[] args) throws CloneNotSupportedException {
+        Student student1 = new Student("Ali", "Veli", 18, 11111111111L, "M");
+       // Student student2 = new Student("Ali", "Veli", 21, 11111111111L, "M");
+        Student student3 = student1.clone();
+        student3.setSecondName("Kaya");
+        student3.setSsid(11111111112L);
+        System.out.println(student1.getSecondName());
+
+        //System.out.println(student1);
+        System.out.println(student1.equals(student3)); // true
+
+    }
+}
